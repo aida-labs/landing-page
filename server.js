d7ac7a76358bedc6dd930564ba0cc64387eed547
Vincent Liu <vinceliu21@gmail.com>
2019-07-12 15:22:31 -0700

    port

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 var path = require('path');
 const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000;
     
 app.prepare()
 .then(() => {
@@ -19,7 +20,7 @@ app.prepare()
     return handle(req, res)
   })
     
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
